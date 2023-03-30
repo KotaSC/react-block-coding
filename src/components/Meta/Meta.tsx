897d629636eb937f0c8d6464b755d92123418cdb
KotaSC <is0302ke@gmail.com>
2023-03-30 21:44:32 +0900

    add google fonts

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -13,6 +13,16 @@ const Meta = () => {
             content="width=device-width, initial-scale=1.0"
           />
           <title>Sample Profile Page</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Philosopher:wght@700&family=Piazzolla:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Helmet>
       </HelmetProvider>
       <Favicon url="https://placehold.jp/16x16.png?text=P" />
